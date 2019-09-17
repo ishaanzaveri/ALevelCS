97f4a2c9fa6fb7ce7ec06794ee2f848605b1cf8d
ishaanzaveri <39041074+ishaanzaveri@users.noreply.github.com>
2019-09-17 09:22:44 +0530

    added CD Class

---
+++ LibraryClass/LibraryClass/CD.vb
@@ -0,0 +1,14 @@
+﻿Public Class CD
+    Inherits LibItem
+    Private Genre As String
+    Public Sub New(Genre As String)
+        MyBase.New
+        Me.Genre = Genre
+    End Sub
+    Sub SetGenre(Genre As String)
+        Me.Genre = Genre
+    End Sub
+    Function GetGenre()
+        Return Me.Genre
+    End Function
+End Class
