239834e321691c6751cf1475d6d2a649d27359d5
ishaanzaveri <39041074+ishaanzaveri@users.noreply.github.com>
2019-09-27 10:59:06 +0530

    Factorial in Recursion initial Commit

---
+++ Recursion/Recursion/Module1.vb
@@ -0,0 +1,17 @@
+﻿Module Module1
+
+    Sub Main()
+        Console.WriteLine("Enter number")
+        Console.WriteLine(Factorial(Console.ReadLine()))
+        Console.ReadLine()
+    End Sub
+
+    Function Factorial(i As Integer)
+        If i = 1 Then
+            Return 1
+        Else
+            Return i * Factorial(i - 1)
+        End If
+    End Function
+
+End Module
