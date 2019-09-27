cc7eba266708bfe3d2ff6c420b535ab9696f2473
ishaanzaveri <39041074+ishaanzaveri@users.noreply.github.com>
2019-09-27 11:14:28 +0530

    Fibbonacci

--- Recursion/Recursion/Module1.vb
+++ Recursion/Recursion/Module1.vb
@@ -3,6 +3,7 @@
     Sub Main()
         Console.WriteLine("Enter number")
         Console.WriteLine(Factorial(Console.ReadLine()))
+        Console.WriteLine(Fibonacci(Console.ReadLine()))
         Console.ReadLine()
     End Sub
 
@@ -13,5 +14,14 @@
             Return i * Factorial(i - 1)
         End If
     End Function
+
+    Function Fibonacci(i As Integer)
+        If i = 1 Or i = 2 Then
+            Return 1
+        Else
+            Return Fibonacci(i - 1) + Fibonacci(i - 2)
+        End If
+    End Function
+
 
 End Module
