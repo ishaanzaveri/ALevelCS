625ddec18d70fc66b208f450bd173e524dffa0e9
ishaanzaveri <39041074+ishaanzaveri@users.noreply.github.com>
2019-09-29 18:58:59 +0530

    made Stack Class

---
+++ Stacks/Stacks/Stack.vb
@@ -0,0 +1,44 @@
+﻿Public Class Stack
+    Private Arr(19) As Integer
+    Private TOS As Integer
+    Public Sub New()
+        TOS = -1
+        For i = 0 To 19
+            Arr(i) = 0
+        Next
+    End Sub
+    Public Function getTOS() As Integer
+        Return TOS
+    End Function
+    Public Function isStackFull() As Boolean
+        If TOS = 19 Then
+            Return True
+        Else
+            Return False
+        End If
+    End Function
+    Public Sub Push(n As Integer)
+        If isStackFull() Then
+            Console.WriteLine("Error Stack is full")
+        Else
+            TOS = TOS + 1
+            Arr(TOS) = n
+        End If
+    End Sub
+    Public Sub Pop()
+        If isStackEmpty() Then
+            Console.WriteLine("Stack empty cant pop")
+        Else
+            Console.WriteLine(Arr(TOS))
+            TOS = TOS - 1
+        End If
+    End Sub
+    Public Function isStackEmpty() As Boolean
+        If TOS = -1 Then
+            Return True
+        Else
+            Return False
+        End If
+    End Function
+
+End Class
